43050f195421bb247e2feb9257b32c616ffde7f4
taykusah <kusahterfa@gmail.com>
2024-06-14 06:49:09 +0100

    Final Commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('THIS IS CSC 314 CLOUD COMPUTING DEVOPS; CI-CD PIPELINE CREATED WITH AWS. THIS PAGE INDICATES SUCCESS. MIKE-KUSAH TERFA VUG/CSC/21/5198.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
